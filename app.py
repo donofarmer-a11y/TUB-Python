4bf62cfebb8bd12e70182dca4a0350e564431ce7
Donno <dono.farmer@gmail.com>
2026-07-17 11:44:17 +0200

    plotly3 again

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ wmiii = pd.pivot_table(
 fig_scores = px.imshow(
     wmiii,
     title="Distribution of scores",
-    color_continuous_scale="thermal",
+    color_continuous_scale="plotly3",
     range_color=[1, wmiii.values.max()],
     height=500,
     width=900,
